9532b3819a942de7ed48db834349cf56f81ab44f
Yuri Ramos <yuriramoscc@gmail.com>
2020-11-17 10:42:19 -0300

    Usando o axios na função de monitoramento, em breve passarei os requests para ele também

--- controllers/devourer.js
+++ controllers/devourer.js
@@ -1,37 +1,46 @@
 const fs = require('fs');
 const moment = require('moment');
 const request = require('request');
+const axios = require('axios');
 const Impressora = require('../models/impressoras');
 const hostname = 'https://api.printwayy.com';
 const printers_path = '/devices/v1/printers';
 const urlBase = `${hostname}${printers_path}`;
 const codename = require('./codename');
-
 const headers = {
     'printwayy-key': '5542C0E2-6C0F-43F1-B576-5056CED690B1'
 };
+const contadores = ['17a99399-3158-4516-ae02-65835302ea6c', '1953dbda-8cd7-44c2-a937-0f0c393b0790',
+    '52a10dda-9d67-41a2-b83e-68c16c59a702', '603cd395-52f5-4819-a412-8248390d079f', 'a13ee0db-a964-48d6-888a-00fe59fc55c5',
+    'b22b8a02-4a89-4beb-a503-ec9cc69a99c4', 'bba45dc4-0582-429c-b9e1-d093d33f2e39', 'cc654080-0664-4bcd-8211-94d1d325c774',
+    'cd42173b-d8bd-44fe-a991-252893fcf4bf'];
 /*
     Essa classe consome a API, por isso esse nome de Devourer.
     Ela recebe os dados enviados em JSON e grava em uma Array,
     essa Array é tratada em sua respectiva classe Model, que
     faz o insert no banco de dados.
 */
+// urlBase+'/'+contadores[row] +'/counters'
+// headers: headers
 class Devourer {
     buscarCounters(res, contadoresResult) {
-        var contadores = ['17a99399-3158-4516-ae02-65835302ea6c', '1953dbda-8cd7-44c2-a937-0f0c393b0790',
-            '52a10dda-9d67-41a2-b83e-68c16c59a702', '603cd395-52f5-4819-a412-8248390d079f', 'a13ee0db-a964-48d6-888a-00fe59fc55c5',
-            'b22b8a02-4a89-4beb-a503-ec9cc69a99c4', 'bba45dc4-0582-429c-b9e1-d093d33f2e39', 'cc654080-0664-4bcd-8211-94d1d325c774',
-            'cd42173b-d8bd-44fe-a991-252893fcf4bf'];
+        // Nesse método, estarei testando/utilizando a library axios, em vez da library 'request',
+        // em breve todos os métodos que  utilizam o request, serão atualizados.
         var contadoresResult = [];
-        for (var row in contadores) {
-            request({ url: urlBase + contadores[row] + '/counters', headers: headers }, (err, req, resp) => {
-                contadoresResult.push({
-                    totalPB: resp[0].totalCount, totalCor: resp[1].totalCount, total:resp[0].totalCount+resp[1].totalCount
-                })
-            });
+        var promises = [];
+        for (var row = 0; row < contadores.length; row++) {
+            promises.push(axios.get(urlBase + '/' + contadores[row] + '/counters', { headers: headers })
+                .then(function (response) {
+                    console.log(contadores[row]);
+                    return response.data[0].totalCount + response.data[1].totalCount;
+                }));
         }
-        console.dir(contadoresResult);
-        return contadoresResult;
+
+        Promise.all(promises).then((result) => {
+            console.dir(result);
+            console.log('\n---------------------------------');
+        });
+        //  return contadoresResult;
     }
 
     requestPrintWayy(skip, impressorasAtualizadas, res) {
