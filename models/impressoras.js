57f1af5ae4243be59bc6b834056be405175f39e0
Yuri Ramos <yuriramoscc@gmail.com>
2020-11-18 10:23:35 -0300

    (Y)

--- models/impressoras.js
+++ models/impressoras.js
@@ -101,7 +101,7 @@ class Impressora {
                         }
                     }
                     else if (impressoraTest.status == 'online' && valores.status == 'offline') {
-                        console.log(valores.serialNumber + 'ficou offline')
+                        console.log(valores.customer+ ' '+ valores.manufacturer+ ' '+ valores.model +' '+valores.serialNumber + ' ficou offline')
                     }
                   //  console.log('...Valores atualizados');
                 }
