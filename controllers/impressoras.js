a795e85ba31058987fa42e226259e1bcc617d381
Yuri Ramos Canário Campos <yuriramoscc@gmail.com>
2020-11-24 11:23:33 -0300

    nao deu o annotations

--- controllers/impressoras.js
+++ controllers/impressoras.js
@@ -1,13 +1,18 @@
 const Impressora = require('../models/impressoras');
+const Anotacoes = require('../models/annotations');
 const Devourer = require('./devourer');
 const listaView = require('../views/printers/lista/lista.marko');
 const formView = require('../views/printers/form/form.marko');
 const countersView = require('../views/printers/counters/counters.marko');
 module.exports = app => {
     app.get('/impressoras', (req, res) => {
-        Impressora.lista(res, function(impressoras){
-            res.marko(listaView, {impressoras:impressoras});
-        });
+        var anotacoes = [];
+      //  Anotacoes.lista(res, function (resultados) {
+     //       anotacoes = resultados;
+            Impressora.lista(res, function (impressoras) {
+                res.marko(listaView, { impressoras: impressoras});
+            });
+     //   });
     })
 
     app.get('/impressoras/:id', (req, res) => {
@@ -24,14 +29,13 @@ module.exports = app => {
     });
 
     app.get('/impressoras-offline', (req, res) => {
-
-        Impressora.listaOffline(res, function(impressoras){
-            res.marko(listaView, {impressoras:impressoras, offline: 'true'});
-        });
+            Impressora.listaOffline(res, function (impressoras) {
+                res.marko(listaView, { impressoras: impressoras, offline: 'true' });
+            });
     });
     //finaliza a edição
     app.post('/impressoras-offline', (req, res) => {
-        const valores = {scan_status: req.body.scan_status, scan_observation: req.body.scan_observation}
+        const valores = { scan_status: req.body.scan_status, scan_observation: req.body.scan_observation }
         console.dir(valores);
         Impressora.altera(req.body.id_way, valores, res);
         res.redirect('/impressoras-offline');
@@ -41,19 +45,18 @@ module.exports = app => {
     app.get('/impressoras-printwayy', (req, res) => {
         var impressorasAtualizadas = [];
         Devourer.tratarDados(impressorasAtualizadas, res);
-    }); 
+    });
 
     //edição dos status
     app.get('/impressoras/form/:id_way', (req, res) => {
-        Impressora.buscaPorId(req.params.id_way, res, function(impressora){
-            res.marko(formView, {impressora: impressora});
-        });
+            Impressora.buscaPorId(req.params.id_way, res, function (impressora) {
+                res.marko(formView, { impressora: impressora});
+            });
     });
 
     app.get('/impressoras-monitoramento', (req, res) => {
-        Devourer.buscarCounters(res, function(contadoresResult){
-            //console.dir(contadoresResult);
-            res.marko(countersView, {impressoras:contadoresResult});
+        Devourer.buscarCounters(res, function (contadoresResult) {
+            res.marko(countersView, { impressoras: contadoresResult });
         });
     });
 }
