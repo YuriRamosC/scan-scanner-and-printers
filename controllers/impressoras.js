e6af4537db21011849d5d183e80b4572b267336b
Santa Rosa Distribuidora <yuriramoscc@gmail.com>
2020-11-30 12:07:33 -0300

    s

--- controllers/impressoras.js
+++ controllers/impressoras.js
@@ -20,6 +20,25 @@ module.exports = app => {
         });
     })
 
+    app.get('/impressoras-offline', (req, res) => {
+        Impressora.listaOffline(res, function (impressoras) {
+            res.marko(listaView, { impressoras: impressoras, offline: 'true' });
+        });
+    });
+
+    app.get('/api-impressoras-offline', (req, res) => {
+        Impressora.listaOffline(res, function (impressoras) {
+            res.status(200).json({ impressoras: impressoras});
+        });
+    });
+    //finaliza a edição
+    app.post('/impressoras-offline', (req, res) => {
+        const valores = { scan_status: req.body.scan_status, scan_observation: req.body.scan_observation }
+        Impressora.altera(req.body.id_way, valores, res);
+        res.redirect('/impressoras-offline');
+    });
+
+
 
     app.get('/impressoras/:id', (req, res) => {
         const id = parseInt(req.params.id)
@@ -32,19 +51,6 @@ module.exports = app => {
 
         Impressora.adiciona(impressora, res);
     });
-
-    app.get('/impressoras-offline', (req, res) => {
-        Impressora.listaOffline(res, function (impressoras) {
-            res.marko(listaView, { impressoras: impressoras, offline: 'true' });
-        });
-    });
-    //finaliza a edição
-    app.post('/impressoras-offline', (req, res) => {
-        const valores = { scan_status: req.body.scan_status, scan_observation: req.body.scan_observation }
-        Impressora.altera(req.body.id_way, valores, res);
-        res.redirect('/impressoras-offline');
-    });
-
 
     app.get('/impressoras-printwayy', (req, res) => {
         var impressorasAtualizadas = [];
