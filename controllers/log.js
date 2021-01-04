e8a7d53ec47127359963eaf1748f7aeee6cd88c3
Santa Rosa Distribuidora <yuriramoscc@gmail.com>
2021-01-04 08:48:34 -0300

    correção no log

--- controllers/log.js
+++ controllers/log.js
@@ -6,7 +6,7 @@ const arrayUpdate = [];
 //Esse array guarda os "gravarComData" de uma Atualização e no final vai ser enviado para a aplicação Cliente
 class Log {
     gravar(message) {
-        fs.writeFile(logPath, '\n'+message, function(erro) {
+        fs.appendFile(logPath, '\n'+message, function(erro) {
             if(erro) {
                 console.log('Erro ao gravar o Log');
             }
